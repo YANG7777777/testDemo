1cacdf0fcd1291a4666609a4be93ba083a93850d
杨佳 <yangjia11416@163.com>
2022-04-27 15:33:04 +0800

    防止按钮多次点击

--- src/main.js
+++ src/main.js
@@ -9,6 +9,21 @@ import vueToPdf from 'vue-to-pdf'
 Vue.use(vueToPdf)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
+// 防止el-button重复点击
+Vue.directive('preventReClick', {
+  inserted(el, binding) {
+    el.addEventListener('click', () => {
+      if (!el.disabled) {
+        el.disabled = true
+        setTimeout(() => {
+          el.disabled = false
+        }, binding.value || 2000)
+      }
+    })
+  }
+});
+
 Vue.use(BaiduMap, {
   ak: 'oLXjVdrvLob6rUoY4XTd98GAatT6vU6l'
 })
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="dashboard">Diashboaerd</div>
+  <div class="dashboard">
+    <div>
+      <el-button type="primary" @click="handleCommit" v-preventReClick>防止多次点击</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'DashBoard'
+  name: 'DashBoard',
+  data() {
+    return {}
+  },
+  methods: {
+    handleCommit() {
+      console.log(123)
+    }
+  },
+  // 局部防止el-button重复点击
+  directives: {
+    preventReClick: {
+      // 指令的定义
+      inserted(el, binding) {
+        el.addEventListener('click', () => {
+          if (!el.disabled) {
+            el.disabled = true
+            setTimeout(() => {
+              el.disabled = false
+            }, binding.value || 2000)
+          }
+        })
+      }
+    }
+  },
 
 }
 </script>
